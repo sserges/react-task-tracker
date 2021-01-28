8084ea8d9244eb28a2cef5de865e57fc00898402
sserges <agounkeserge@gmail.com>
2021-01-28 22:25:51 +0000

    Events

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,15 @@ import Button from './Button'
 
 
 const Header = (props) => {
+
+    const onClick = () => {
+        console.log('Click')
+    }
+
     return (
         <header className='header'>
             <h1>{props.title}</h1>
-            <Button color='green' text='Add' />
+            <Button color='green' text='Add' onClick={onClick} />
         </header>
     )
 }
