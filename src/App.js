9465239329958d5aec6f8f9eb4a1d4cb5a576d90
sserges <agounkeserge@gmail.com>
2021-02-16 20:37:38 +0000

    Optional message if no tasks

--- src/App.js
+++ src/App.js
@@ -36,7 +36,11 @@ const App = () => {
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} onDelete={deleteTask} />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} />
+      ) : (
+          'No Tasks To Show'
+        )}
     </div>
   );
 }
