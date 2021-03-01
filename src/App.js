cbca522480d9f73bbe048eda49b011bcea353245
sserges <agounkeserge@gmail.com>
2021-03-01 22:13:42 +0000

    Toggle reminder on server

--- src/App.js
+++ src/App.js
@@ -25,6 +25,14 @@ const App = () => {
     return data
   }
 
+  // Fetch 
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json()
+
+    return data
+  }
+
   // Add Task
   const addTask = async (task) => {
     const res = await fetch(
@@ -59,10 +67,23 @@ const App = () => {
   }
 
   // Toggle Reminder
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id)
+    const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(updTask)
+    })
+
+    const data = await res.json()
+
     setTasks(
       tasks.map(
-        (task) => task.id === id ? { ...task, reminder: !task.reminder } : task
+        (task) => task.id === id ? { ...task, reminder: data.reminder } : task
       )
     )
   }
