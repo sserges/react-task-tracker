8ceab0fe1475a539a09520b525c4fb1ffb631cda
sserges <agounkeserge@gmail.com>
2021-12-05 15:07:11 -0400

    updated to react router v6

--- src/components/TaskDetails.js
+++ src/components/TaskDetails.js
@@ -1,12 +1,16 @@
 import { useState, useEffect } from 'react'
-import { useParams, Navigate } from 'react-router-dom'
+import { useParams, useNavigate, useLocation } from 'react-router-dom'
+
+import Button from './Button'
 
 function TaskDetails() {
   const [loading, setLoading] = useState(true)
   const [task, setTask] = useState({})
-  const [error, setError] = useState(null)
+  //   const [error, setError] = useState(null)
 
   const params = useParams()
+  const navigate = useNavigate()
+  const { pathname } = useLocation()
 
   useEffect(() => {
     const fetchTask = async () => {
@@ -14,7 +18,7 @@ function TaskDetails() {
       const data = await res.json()
 
       if (res.status === 404) {
-        setError('Task not found')
+        navigate('/')
       }
 
       setTask(data)
@@ -22,18 +26,21 @@ function TaskDetails() {
     }
 
     fetchTask()
-  })
-
-  if (error) {
-    return <Navigate to='/' />
-  }
+  }, [params.id, navigate])
 
   return loading ? (
     <h3>Loading...</h3>
   ) : (
     <div>
+      <p>{pathname}</p>
       <h3>{task.text}</h3>
       <p>{task.day}</p>
+      <Button
+        onClick={() => {
+          navigate(-1)
+        }}
+        text='Go Back'
+      />
     </div>
   )
 }
